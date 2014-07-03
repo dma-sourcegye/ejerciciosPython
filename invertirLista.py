cff11f4387f48cfd4e9715bf76fb08414f7d8768
dma-source <dmasourcegye@gmail.com>
2014-07-03 00:09:51 -0500

    Update invertirLista.py

--- invertirLista.py
+++ invertirLista.py
@@ -1,28 +1,29 @@
 from os import system
 lista=[]#Lista principal
-lista2=[]#lista donde se almacenaran los elementos inevrtidos
+lista2=[]#lista donde se almacenaran los elementos invertidos
 lista3=[] #pila que servirá para invertir los valores de la lista
 
 
-def GenerarLista(lista,lista2,lista3):
+def InvertirLista(lista,lista2,lista3): #Funcion  inversora
 	op=""
 	while op!="n":
-		system("cls")
-		itemLista=int(raw_input("Ingrese un elemento: "))
-		lista.append(itemLista)
-		lista3.append(itemLista)
-		op=raw_input("desea continuar (s/n): ")
+		system("cls") #limpia la pantalla de la consola se pasa como parametro cls si el script se ejecuta en windows
+		itemLista=int(raw_input("Ingrese un elemento: ")) #se pide ingresar un elemento para nuestra lista el cual sera del tipo entero
+		lista.append(itemLista) #se agrega el elemento a la lista
+		lista3.append(itemLista)#se agrega el mismo element a la pila
+		op=raw_input("desea continuar (s/n): ") #el script nos pregunta si deseamos continuar con el ingreso de elementos
 
-	print "Elementos de la lista generada: ", lista
+	print "Elementos de la lista generada: ", lista #imprimimos todos los elementos del objeto lista
 	for item in lista:
-		lista2.append(lista3.pop())
-	print "lista ivertida: ",lista2
+		lista2.append(lista3.pop()) #se almacenan lso elementos de manera invertida en una nueva lista
+	print "lista ivertida: ",lista2     #presentamos la nueva lista
 
+#como opcion nos dan a elegir si se desea eliminar la primera lista ya que esta no nos servira mas
 	op=raw_input("Desa eliminar la lista (s/n)? ")
 	if(op=="s"):
-		del (lista)
+		del (lista) 	#eliminamos la lista
 		print "lista eliminada"
 	else:
 		print "no se elimino la lista"
 
-GenerarLista(lista,lista2,lista3)
+InvertirLista(lista,lista2,lista3)  	#llamamos a la funcion InvertirLista
