dec2fe51cea732d7e6d10a96c36df36d68faeb28
dma-source <dmasourcegye@gmail.com>
2014-07-03 00:12:42 -0500

    Create numeroMayor.py
    elige el elemento mayor del arreglo de enteros

---
+++ numeroMayor.py
@@ -0,0 +1,15 @@
+print "Introduce 5 numeros:"
+vector=[] #definimos una lista
+#introducimos 5 numeros
+for i in range(0,5):
+    numero=int(raw_input('introduce no.  '+str(i+1)+': '))
+    vector.append(numero)
+
+print vector
+mayor=0
+
+for v in vector:
+    if mayor<v:
+        mayor=v
+
+print 'mayor es: ',mayor
